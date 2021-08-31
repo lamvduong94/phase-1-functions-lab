b0e48b677919b8ec0bc2af46e8813de7a92eac14
Lam <LamDuong.inbox@gmail.com>
2021-08-31 09:46:16 -0700

    commit tree 21f300e63b87693216785b2f946456679af05130

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    return Math.abs(num - 42);
+}
+
+function distanceFromHqInFeet(num) {
+    return Math.abs((num - 42) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    const travelled = Math.abs((start - destination) * 264);
+    if (travelled <= 400) {
+        return 0;
+    } else if (travelled > 400 && travelled <= 2000) {
+        return (travelled - 400) * 0.02;
+    } else if (travelled > 2000 && travelled <= 2500) {
+        return 25;
+    } else if (travelled > 2500) {
+        return 'cannot travel that far';
+    }
+}
